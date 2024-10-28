8bd489d1d2761ec3dcb56320abab0b8169217ccd
Colin Jamison <cjamison@valmagus.com>
2024-10-28 13:08:24 -0700

    Program 3 Init
    Just a raw copy of Program 2

---
+++ prgm03/main.c
@@ -0,0 +1,261 @@
+#include <arpa/inet.h>
+#include <bits/types/struct_iovec.h>
+#include <complex.h>
+#include <dirent.h>
+#include <netinet/in.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "utilities.h"
+
+// Wish we had C23 on jaguar. Could specify enum size.
+// `enum Action : uint8_t {` my beloved.
+enum Action {
+  JOIN = 0,
+  PUBLISH,
+  SEARCH,
+};
+
+typedef struct {
+  uint32_t peer_id;
+} JoinBody;
+
+typedef struct {
+  uint32_t count;
+  string* filenames;
+} PublishBody;
+
+typedef struct {
+  string search_term;
+} SearchBody;
+
+// Thanks to padding, the bit layout here will not match our wire format.
+// We'll still need to memcpy into a byte buffer.
+// Though there's always __attribute__((packed))...
+// Of course, that doesn't account for endianness.
+// ¯\_(ツ)_/¯
+typedef struct {
+  enum Action tag;
+
+  union {
+    JoinBody join;
+    PublishBody publish;
+    SearchBody search;
+  } body;
+} Packet;
+
+typedef struct {
+  uint8_t* buf;
+  size_t len;
+} NetBuffer;
+
+/**
+ * Represents a response to a search query.
+ * If all fields are zero, the file was not found.
+ * IP is stored in network byte order.
+ */
+typedef struct {
+  uint32_t peer_id;
+  uint32_t ip;
+  uint16_t port;
+} SearchResponse;
+
+/**
+ * @brief Performs a peer-to-peer search based on the given search term.
+ *
+ * This function initiates a search to the registry for the given search term.
+ *
+ * @param search_term The term to search for in the peer-to-peer network.
+ * @param s Network socket to send the search request on.
+ * @return SearchResponse The result of the search, encapsulated in a SearchResponse structure.
+ */
+SearchResponse p2p_search(string search_term, int s);
+
+/**
+ * Returns a pointer to an allocated buffer that contains
+ * the network representation of a Packet.
+ */
+NetBuffer packet_to_netbuf(Packet packet);
+
+void dump_packet(const NetBuffer* packet) {
+  for (size_t i = 0; i < packet->len; i++) {
+    printf("%02x ", packet->buf[i]);
+  }
+  printf("\n");
+}
+
+int main(int argc, char* argv[]) {
+  if (argc < 4) {
+    fprintf(stderr, "Usage: %s <registry> <port_number> <peer_id>\n", argv[0]);
+    return (EXIT_FAILURE);
+  }
+
+  // Is there a way to check this without a conversion we immediately throw away?
+  int port = atoi(argv[2]);
+  if (port < 2000 || port > 65535) {
+    fprintf(stderr,
+            "Invalid port number. Port must be between 2000 and 65535 "
+            "inclusive.\n");
+    return (EXIT_FAILURE);
+  }
+
+  // Signedness issue. TODO: Fix.
+  uint32_t peer_id = atoi(argv[3]);
+
+  if (peer_id <= 0) {
+    fprintf(stderr, "Invalid peer id: \"%d\". Exiting.\n", peer_id);
+    return (EXIT_FAILURE);
+  }
+
+  int s;
+
+  if ((s = lookup_and_connect(argv[1], argv[2])) < 0) {
+    fprintf(stderr, "Unable to connect to host \"%s\". Exiting.\n", argv[1]);
+    return (EXIT_FAILURE);
+  }
+
+  int exit = 0;
+
+  while (!exit) {
+    printf("Please enter a command, one of JOIN, SEARCH, PUBLISH, or EXIT: ");
+    string cmd_input = readline();
+
+    // printf("%s\n", cmd_input);
+
+    if (strcmp(cmd_input.buf, "EXIT") == 0) {
+      exit = 1;
+      break;
+    }
+
+    if (strcmp(cmd_input.buf, "JOIN") == 0) {
+      Packet packet = {.tag = JOIN, .body.join = {.peer_id = peer_id}};
+      NetBuffer nb = packet_to_netbuf(packet);
+
+      // dump_packet(&nb);
+      send_all(s, nb.buf, nb.len);
+      free(nb.buf);
+    }
+
+    if (strcmp(cmd_input.buf, "SEARCH") == 0) {
+      printf("Please enter a filename to search for: ");
+      string search_term = readline();
+
+      SearchResponse response = p2p_search(search_term, s);
+
+      if (response.peer_id == 0) {
+        printf("File not indexed by registry.\n");
+      } else {
+        printf("File found at\n");
+        char peer_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &response.ip, peer_ip, INET_ADDRSTRLEN);
+        printf("\tPeer %u\n", response.peer_id);
+        printf("\t%s:%d\n", peer_ip, response.port);
+      }
+    }
+
+    if (strcmp(cmd_input.buf, "PUBLISH") == 0) {
+      string* files = NULL;
+      int32_t count = read_files(&files);
+
+      if (count < 0) {
+        fprintf(stderr, "Failed to read files. Exiting.\n");
+        return (EXIT_FAILURE);
+      }
+
+      Packet packet = {.tag = PUBLISH, .body.publish = {.count = count, .filenames = files}};
+      NetBuffer nb = packet_to_netbuf(packet);
+
+      // dump_packet(&nb);
+      send_all(s, nb.buf, nb.len);
+    }
+
+    free(cmd_input.buf);
+  }
+
+  return 0;
+}
+
+SearchResponse p2p_search(string search_term, int s) {
+  Packet packet = {.tag = SEARCH, .body.search = {.search_term = search_term}};
+  NetBuffer nb = packet_to_netbuf(packet);
+
+  // dump_packet(&nb);
+  ssize_t sent = send_all(s, nb.buf, nb.len);
+  if (sent < 0) {
+    fprintf(stderr, "Failed to send search term. Exiting.\n");
+    return (SearchResponse){.peer_id = 0};
+  }
+  free(nb.buf);
+
+  uint8_t response_buf[10];
+  ssize_t received = recv_all(s, response_buf, 10);
+
+  if (received < 0) {
+    fprintf(stderr, "Failed to receive search response. Exiting.\n");
+    return (SearchResponse){.peer_id = 0};
+  }
+
+  SearchResponse response;
+  memcpy(&response.peer_id, response_buf, sizeof(uint32_t));
+  memcpy(&response.ip, response_buf + sizeof(uint32_t), sizeof(uint32_t));
+  memcpy(&response.port, response_buf + (2 * sizeof(uint32_t)), sizeof(uint16_t));
+  response.peer_id = ntohl(response.peer_id);
+  response.port = ntohs(response.port);
+
+  return response;
+}
+
+// This would be a lot easier if we used a packed struct.
+NetBuffer packet_to_netbuf(Packet packet) {
+#define qcopy(dst, x) memcpy(dst, &x, sizeof(x));
+  size_t size = sizeof(uint8_t);
+  switch (packet.tag) {
+    case JOIN:
+      size += sizeof(packet.body.join.peer_id);
+      break;
+    case PUBLISH:
+      size += sizeof(packet.body.publish.count);
+      for (uint32_t i = 0; i < packet.body.publish.count; i++) {
+        size += packet.body.publish.filenames[i].len;
+      }
+      break;
+    case SEARCH:
+      size += packet.body.search.search_term.len;
+  }
+
+  uint8_t* buffer = (uint8_t*)malloc(size);
+  if (buffer == NULL) {
+    return (NetBuffer){.buf = NULL, .len = 0};
+  }
+
+  uint8_t* ptr = buffer;
+  memcpy(ptr, &packet.tag, sizeof(uint8_t));
+  ptr += sizeof(uint8_t);
+
+  switch (packet.tag) {
+    case JOIN: {
+      uint32_t peer_id = htonl(packet.body.join.peer_id);
+      qcopy(ptr, peer_id);
+      break;
+    }
+    case PUBLISH: {
+      uint32_t count = htonl(packet.body.publish.count);
+      qcopy(ptr, count);
+      ptr += sizeof(count);
+      for (uint32_t i = 0; i < packet.body.publish.count; i++) {
+        ptrdiff_t len = packet.body.publish.filenames[i].len;
+        memcpy(ptr, packet.body.publish.filenames[i].buf, len);
+        ptr += len;
+      }
+      break;
+    }
+    case SEARCH:
+      memcpy(ptr, packet.body.search.search_term.buf, packet.body.search.search_term.len);
+      break;
+  }
+
+  return (NetBuffer){.buf = buffer, .len = size};
+}
---
+++ prgm03/utilities.c
@@ -0,0 +1,135 @@
+#include "utilities.h"
+
+#include <stdint.h>
+
+ssize_t recv_all(int socket, uint8_t* buff, ssize_t len) {
+  ssize_t bytes_received;
+  ssize_t total_received = 0;
+  ssize_t leftover = len;
+
+  while (leftover > 0) {
+    bytes_received = recv(socket, buff + total_received, leftover, 0);
+    if (bytes_received < 0) {
+      // Return the error code
+      return bytes_received;
+    } else if (bytes_received == 0) {
+      // Connection closed
+      break;
+    }
+    total_received += bytes_received;
+    leftover -= bytes_received;
+  }
+
+  return total_received;
+}
+
+ssize_t send_all(int s, uint8_t* buf, ssize_t len) {
+  ssize_t total = 0;
+  ssize_t bytesleft = len;
+  ssize_t n;
+
+  while (total < len) {
+    n = send(s, buf + total, bytesleft, 0);
+    if (n == -1) {
+      // Return the error code
+      return n;
+    } else if (n == 0) {
+      // Connection closed
+      break;
+    }
+    total += n;
+    bytesleft -= n;
+  }
+
+  return total;
+}
+
+int lookup_and_connect(const char* host, const char* service) {
+  struct addrinfo hints;
+  struct addrinfo *rp, *result;
+  int s;
+
+  /* Translate host name into peer's IP address */
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = 0;
+  hints.ai_protocol = 0;
+
+  if ((s = getaddrinfo(host, service, &hints, &result)) != 0) {
+    fprintf(stderr, "stream-talk-client: getaddrinfo: %s\n", gai_strerror(s));
+    return -1;
+  }
+
+  /* Iterate through the address list and try to connect */
+  for (rp = result; rp != NULL; rp = rp->ai_next) {
+    if ((s = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol)) == -1) {
+      continue;
+    }
+
+    if (connect(s, rp->ai_addr, rp->ai_addrlen) != -1) {
+      break;
+    }
+
+    close(s);
+  }
+  if (rp == NULL) {
+    perror("stream-talk-client: connect");
+    return -1;
+  }
+  freeaddrinfo(result);
+
+  return s;
+}
+
+string readline() {
+  size_t len = 0;
+  ssize_t read = 0;
+  char* buf = NULL;
+  read = getline(&buf, &len, stdin);
+
+  if (read == -1) {
+    perror("getline");
+    free(buf);
+    return (string){.buf = NULL, .len = 0};
+  }
+
+  // Replace the trailing newline from getline
+  if (buf[read - 1] == '\n') {
+    buf[read - 1] = '\0';
+  }
+
+  string out = (string){.buf = buf, .len = read};
+  return out;
+}
+
+int32_t read_files(string** out) {
+  DIR* dir = opendir("SharedFiles");
+
+  if (dir == NULL) {
+    fprintf(stderr, "Unable to open directory \"SharedFiles\". Exiting.\n");
+    return -1;
+  }
+
+  struct dirent* entry;
+  uint32_t count = 0;
+
+  while ((entry = readdir(dir))) {
+    // Ignore directories
+    if (entry->d_type == DT_REG) {
+      if (*out == NULL) {
+        *out = malloc(sizeof(string));
+      } else {
+        *out = realloc(*out, (count + 1) * sizeof(string));
+      }
+
+      string filename = (string){.buf = strdup(entry->d_name), .len = strlen(entry->d_name) + 1};
+      filename.buf[filename.len - 1] = '\0';
+      (*out)[count] = filename;
+      count++;
+    }
+  }
+
+  closedir(dir);
+  return count;
+}
