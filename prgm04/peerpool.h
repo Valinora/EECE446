eea30b2848ce04a614a590033743daa0b8102a08
Colin Jamison <cjamison@fastmail.com>
2024-12-11 15:47:11 -0800

    Move the PriorityQueueExt into peerpool, delete unneeded file

--- prgm04/peerpool.h
+++ prgm04/peerpool.h
@@ -10,12 +10,39 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <optional>
+#include <algorithm>
 
 #include "netdb.h"
 #include "peer.h"
-#include "pqueue.h"
 
 #define MAX_PENDING 5
+
+class PriorityQueueExt : public std::priority_queue<int> {
+ public:
+  /**
+   * Removes a given value from the queue, regardless of position, and returns the value.
+   * @param value The value to be removed.
+   * @return A std::optional containing the value removed or nullopt if no match was found.
+   */
+  inline std::optional<int> remove(const int& value) {
+    auto it = std::find(this->c.begin(), this->c.end(), value);
+
+    if (it == this->c.end()) {
+      return std::nullopt;
+    } else if (it == this->c.begin()) {
+      int ret = this->top();
+      this->pop();
+      return ret;
+    } else {
+      int ret = *it;
+      this->c.erase(it);
+      std::make_heap(this->c.begin(), this->c.end(), this->comp);
+      return ret;
+    }
+  }
+};
 
 /**
  * Create, bind and passive open a socket on a local interface for the provided service.
